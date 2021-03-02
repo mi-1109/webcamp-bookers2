63b85fd98d466d56d817df90fbe14773541b968b
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-02 08:04:02 +0000

    [Add] Installed devise and added associated Model and View

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  #devise利用機能が使われる前に、パラメータを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  #ユーザ登録時にnameのデータ操作を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end

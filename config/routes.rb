c547e2504f8d4fc8eef58bf70b224735b30de5a8
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-03 11:09:42 +0000

    [Add] edit & update actions on books_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :books, only: [:create, :index, :show, :edit, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 
   delete '/books/:id', to: 'books#destroy', as: 'destroy_book'

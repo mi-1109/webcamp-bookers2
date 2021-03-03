8cac1eab2807fa013b6b5af9bd7b335f606d30f5
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-03 05:13:49 +0000

    [Fix] Deleted UserImage Model

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   resources :books, only: [:create, :index, :show, :edit, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
+  resources :user_images, only: [:create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_022415) do
+ActiveRecord::Schema.define(version: 2021_03_02_091043) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "user_images", force: :cascade do |t|
-    t.string "image_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

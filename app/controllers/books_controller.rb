071b966f4c2460b0c4487532f337cd8db607f7ad
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-03 10:58:19 +0000

    [Add] Books-show and Users-show Views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.new
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy

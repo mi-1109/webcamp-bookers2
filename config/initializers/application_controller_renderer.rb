6ade2de4cd66825a55e6506eec93fc12d079ae69
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-03 05:25:33 +0000

    [Update] Refile Setting -enabled profile image upload

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f220008ee1b89057d765629ceea36c980b3275c0bf19724418d0cf29b6dd3c8dd326425c032a0da3c991cbd32a30c60b621c4994aebd9d19f088adc76787dc1e'

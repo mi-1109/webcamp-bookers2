af79d5417f7e3a62e4e1b77d6dc1675bac92ff2a
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-03 03:14:54 +0000

    [Add] Update-action to user_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root to: 'homes#top'
 
   resources :books, only: [:create, :index, :show, :edit, :destroy]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 end

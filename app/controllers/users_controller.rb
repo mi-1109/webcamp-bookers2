682d52a76f26b40c529ddae7bd832abe50bf4d5c
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-04 07:10:47 +0000

    [Add] flash message 'successfully' to posts and edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
 
-  
-
   def index
     @book = Book.new
     @users = User.all
@@ -20,8 +18,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "successfully updated User Info"
+    else
+      render action: :edit
+    end
   end
 
   private

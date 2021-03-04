5193e52b1037495167d393a6af7d76267dace9db
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-04 06:11:33 +0000

    [Add] Validations to User and Book models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-  #devise利用機能が使われる前に、パラメータを実行
-  before_action :authenticate_user!,except: [:top, :collection]
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
+#devise利用機能が使われる前に、パラメータを実行
+before_action :authenticate_user!,except: [:top, :collection]
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+#ログイン後/サインアップ後のリダイレクト先
 def after_sign_in_path_for(resource)
   user_path(current_user.id)
 end
@@ -11,12 +14,12 @@ def after_sign_out_path_for(resource)
   root_path
 end
 
-  protected
+#ユーザ登録時にnameとemailのデータ操作を許可
+protected
 
-  #ユーザ登録時にnameとemailのデータ操作を許可
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+end
 
 end

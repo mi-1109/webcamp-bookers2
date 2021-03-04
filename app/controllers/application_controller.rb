7d5fe75b3ff57eed055f7e620de86ed1ab4df615
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-04 02:51:23 +0000

    [Update] path for after sign-in/sign-out on application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,22 @@
 class ApplicationController < ActionController::Base
   #devise利用機能が使われる前に、パラメータを実行
-  before_action :authenticate_user!,except: [:top]
-  before_action :authenticate_user!,except: [:collection]
+  before_action :authenticate_user!,except: [:top, :collection]
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+def after_sign_in_path_for(resource)
+  user_path(current_user.id)
+end
+
+def after_sign_out_path_for(resource)
+  root_path
+end
 
   protected
 
-  #ユーザ登録時にnameのデータ操作を許可
+  #ユーザ登録時にnameとemailのデータ操作を許可
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
 end

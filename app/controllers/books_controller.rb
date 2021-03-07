08a35c286302978061c6890e40681d19288cdc4d
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-07 07:02:10 +0000

    [Fix] added @user declaration to create-action of books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @user = User.find(@book.user_id)
     if @book.save
       redirect_to book_path(@book.id), notice: "successfully registered your post!"
     else

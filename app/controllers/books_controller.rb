647b270b4743f1e87e2aa522bcb9efe424aaa9a0
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-07 05:14:34 +0000

    [Fix] Redirect path of destroy action on books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path(book.id), notice: "successfully deleted"
+    redirect_to books_path, notice: "successfully deleted"
   end
 
   private

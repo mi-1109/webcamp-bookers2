4a08b1cd1a73257d32fc853f8a894a4ff6e1850b
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-02 05:49:05 +0000

    [Add] コメント削除機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
+    comment = current_user.book_comments.new(book_comment_params) #ログインユーザのコメントをnewする。newするときにbook_comment_paramsの値が入る
     comment.book_id = book.id
     comment.save
     redirect_to book_path(book)
   end
 
   def destroy
+    BookComment.find_by(id:params[:id], book_id:params[:book_id]).destroy
+    redirect_to book_path(params[:book_id])
   end
 
   private

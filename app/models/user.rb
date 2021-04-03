a4e10af405b6205b8085002817d893aa6a239143
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-03 03:16:20 +0000

    [Add] Comments on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォローするユーザ取得。follower_idにuser_idを格納。
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォローされるユーザ取得。followed_idにuser_idを格納。
 
+  #以下、@user.followingsとした際に、「@userがフォローしているUser一覧」を返すための記述
   #自分がフォローしているユーザと、自分をフォローしているユーザをthroughを使った関連付けで取得する
   has_many :following_user, through: :follower, source: :followed #自分がフォローしているユーザ
   has_many :follower_user, through: :followed, source: :follower  #自分をフォローしているユーザ
